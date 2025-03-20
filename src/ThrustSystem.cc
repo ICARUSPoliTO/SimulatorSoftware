a969620a7dfc7d1b4263597fc05780ab9c4521f2
Yognda <117925895+GiovanniTortia@users.noreply.github.com>
2025-03-20 18:32:38 +0100

    Add files via upload

--- src/ThrustSystem.cc
+++ src/ThrustSystem.cc
@@ -73,6 +73,14 @@ void ThrustSystem::Configure(
 	//Enable velocity checks or WorldAngularVelocity(...) returns nullopt
 	this->dataPtr->body_link.EnableVelocityChecks(_ecm, true);
 	
+	// Check if the propeller's diameter was specified and read the value
+	if(!_sdf -> HasElement("prop_diameter")){
+		gzerr << "Diameter of the propeller missing!" <<std::endl;
+		return;		
+	}
+	std::string prop_diam = _sdf->Get<std::string>("prop_diameter");
+	dataPtr->prop_diam = std::stod(prop_diam)/100.0;
+	
 	// Read the pose of the body
 	this->dataPtr->pose = _ecm.Component<gz::sim::components::WorldPose>(this->dataPtr->body_link.Entity())->Data();
 	
@@ -84,7 +92,6 @@ void ThrustSystem::Configure(
 	else{
 		this->dataPtr->RPM = ang_vel->Data()[0] * 60/(2*3.1415926);
 	}
-	gzdbg <<"RPM: " <<this->dataPtr->RPM <<std::endl;
 	
 	// Read body's velocity vector (world's frame of reference)
 	this->dataPtr->lin_vel = _ecm.Component<gz::sim::components::WorldLinearVelocity>(this->dataPtr->body_link.Entity())->Data();
@@ -92,22 +99,67 @@ void ThrustSystem::Configure(
 	gzlog <<"ThrustSystem configured correctly" <<std::endl;
 }
 
+// DATA RELATIVE TO APC 20.5x20.5E
+double ThrustSystem::interpolate_Kt(double adv_ratio){
+	double j_known[] = {0, 0.0396, 0.0793, 0.1189, 0.1585, 0.1982, 0.2378, 0.2774, 0.3171, 0.3567, 0.3963, 0.4360, 0.4756, 0.5153, 0.5549, 0.5945, 0.6342, 0.6738, 0.7134, 0.7531, 0.7927, 0.8323, 0.8720, 0.9116, 0.9512, 0.9909, 1.0305, 1.0701, 1.1098, 1.1494};
+	
+	double Kt_known[] = {0.1087, 0.1084, 0.1080, 0.1075, 0.1070, 0.1064, 0.1057, 0.1048, 0.1036, 0.1023, 0.1006, 0.0986, 0.0962, 0.0934, 0.0901, 0.0863, 0.0821, 0.0772, 0.0719, 0.0661, 0.0599, 0.0535, 0.0469, 0.0402, 0.0333, 0.0265, 0.0196, 0.0129, 0.0063, 0};
+	
+	int N = 30;
+	
+	if(adv_ratio > j_known[N-1]){
+		return 0;
+	}
+	
+	// Find index of closest j_known
+	int ind=0;
+	while(ind < N && adv_ratio < j_known[ind])
+		ind++;
+		
+	if(j_known[ind]==adv_ratio){
+		return Kt_known[ind];
+	}
+	
+	double slope = (Kt_known[ind+1]-Kt_known[ind])/(j_known[ind+1]-j_known[ind]);
+	double K_t = Kt_known[ind] + (adv_ratio-j_known[ind])*slope;
+	
+	return K_t;
+}
+
 void ThrustSystem::PreUpdate(const gz::sim::UpdateInfo &_info,
                 gz::sim::EntityComponentManager &_ecm)
 {	
-	//gzdbg<<"PreUpdate\n";
+	double IAS = dataPtr->lin_vel.Length();
 	
-	// Compute thrust magnitude from speed and rpm (temporary)
-	double force = dataPtr->RPM * 1/(this->dataPtr->lin_vel.Length()*0.5 + 1);
-	gz::math::Vector3<double> bodyForce(force, 0, 0);
+	// Apply threshold to avoid nonsensical values
+	/*if(IAS < 1e3){
+		IAS = 0;
+	}*/
 	
-	gzdbg<<"RPM: " <<dataPtr->RPM <<std::endl;
+	double adv_ratio;
+	if(IAS == 0 || dataPtr->RPM == 0){
+		adv_ratio = 0;
+	}
+	else{
+		adv_ratio = IAS*60/dataPtr->RPM;
+	}
 	
-	// Express force vector in the world's frame of reference
+	double air_density = 1.225;
+	//double K_t = interpolate_Kt(adv_ratio);
+	
+	//gzdbg <<"IAS: " <<IAS <<" RPM: " <<dataPtr->RPM <<" J: " <<adv_ratio <<std::endl;
+	
+	double K_t = interpolate_Kt(adv_ratio);	
+	
+	
+	double thrust = K_t * air_density * dataPtr->RPM*dataPtr->RPM/3600 * dataPtr->prop_diam*dataPtr->prop_diam*dataPtr->prop_diam*dataPtr->prop_diam;
+	gz::math::Vector3<double> bodyForce(thrust, 0, 0);
+	
+	// Express thrust vector in the world's frame of reference
 	gz::math::Vector3<double> worldForce = this->dataPtr->pose.Rot().RotateVector(bodyForce);
 	
-	// Apply the force
-	//gzdbg <<"About to apply force with value: " <<worldForce.Length() <<std::endl;
+	// Apply the thrsut
+	gzdbg <<"IAS: " <<IAS <<" RPM: " <<dataPtr->RPM <<" J: " <<adv_ratio <<" Kt: " <<K_t <<" Thrust: " <<thrust <<std::endl;
 	this->dataPtr->body_link.AddWorldForce(_ecm, worldForce);
 }
 
@@ -124,6 +176,10 @@ void ThrustSystem::PostUpdate(const gz::sim::UpdateInfo &_info,
 	}
 	else{
 		this->dataPtr->RPM = ang_vel->Data()[0] * 60/(2*3.1415926);
+		
+		// Apply threshold to avoid nonsensical values
+		if(dataPtr->RPM < 1e3)
+			dataPtr->RPM = 0;
 	}
 	
 	// Read body's velocity vector (world's frame of reference)
